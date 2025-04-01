f45ac1378a28507858fcef0ce92077be04f0236d
khoane03 <khoatk4444@gmail.com>
2025-04-01 16:40:13 +0700

    add emailDto

--- src/main/java/com/dev/social/dto/request/auth/PasswordRecoveryRequestDTO.java
+++ src/main/java/com/dev/social/dto/request/auth/PasswordRecoveryRequestDTO.java
@@ -1,5 +1,6 @@
 package com.dev.social.dto.request.auth;
 
+import com.dev.social.utils.validation.emailValidation.EmailConstraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -10,6 +11,9 @@ import lombok.experimental.FieldDefaults;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PasswordRecoveryRequestDTO {
+    @NotBlank(message = "Email is required")
+    @EmailConstraint(message = "Email must be in the form (A-Z,a-z,0-9)@gmail.com")
+    String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
