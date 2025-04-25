90eaa3d43639cc5bde06a60e3704191f8316d5b5
khoane03 <khoatk4444@gmail.com>
2025-04-25 17:06:16 +0700

    add attribute

--- src/main/java/com/dev/social/dto/result/CommentResult.java
+++ src/main/java/com/dev/social/dto/result/CommentResult.java
@@ -1,12 +1,14 @@
 package com.dev.social.dto.result;
 
 
+import java.time.LocalDateTime;
+
 public interface CommentResult {
     String getId();
     String getPostId();
     String getName();
     String getAvatarUrl();
     String getContents();
-    String getCreatedAt();
+    LocalDateTime getCreatedAt();
 
 }
