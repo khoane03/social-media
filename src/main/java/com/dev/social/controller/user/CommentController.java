f8e2cfdb671104351305514d49e570b577c6b13e
khoane03 <khoatk4444@gmail.com>
2025-04-25 17:05:51 +0700

    fix response

--- src/main/java/com/dev/social/controller/user/CommentController.java
+++ src/main/java/com/dev/social/controller/user/CommentController.java
@@ -4,6 +4,7 @@ import com.dev.social.dto.result.CommentResult;
 import com.dev.social.dto.request.user.CommentRequestDTO;
 import com.dev.social.dto.response.ApiResponseDTO;
 import com.dev.social.service.user.CommentService;
+import com.dev.social.utils.constants.AppConst;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -22,17 +23,17 @@ public class CommentController {
     @PostMapping()
     public ApiResponseDTO<?> addComment(@RequestBody CommentRequestDTO req) {
         commentService.addComment(req);
-        return ApiResponseDTO.build();
+        return ApiResponseDTO.of(AppConst.SUCCESS);
     }
 
     @GetMapping("/{id}")
     public ApiResponseDTO<List<CommentResult>> getCommentsByPostId(@PathVariable("id") String id) {
-        return ApiResponseDTO.build(commentService.getCommentsByPostId(id));
+        return ApiResponseDTO.of(commentService.getCommentsByPostId(id));
     }
 
     @DeleteMapping("/{cmtId}")
     public ApiResponseDTO<String> deleteComment(@PathVariable("cmtId") String cmtId) {
         commentService.deleteComment(cmtId);
-        return ApiResponseDTO.build();
+        return ApiResponseDTO.of(AppConst.SUCCESS);
     }
 }
