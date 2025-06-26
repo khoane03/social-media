a03a94b80075cdcae0b4f7914c00e5922140fca0
khoane03 <khoatk4444@gmail.com>
2025-06-26 23:45:25 +0700

    fix mobile

--- src/pages/chat/ChatBox.tsx
+++ src/pages/chat/ChatBox.tsx
@@ -146,8 +146,7 @@ const ChatBox = ({ userId }: ChatBoxProps) => {
 
   return (
     <div
-      className={`bg-white flex flex-col w-full
-    fixed md:static top-[64px] inset-0 z-30 md:rounded-2xl md:shadow-xl 
+      className={`bg-white flex flex-col w-full fixed md:static top-[64px] inset-0 z-30 md:rounded-2xl md:shadow-xl 
     border border-gray-200 overflow-hidden my-0 md:my-2`}
     >
       {/* Header */}
