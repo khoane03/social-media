9cde60e3fd95ec9f7d01c9448bcffda397456ef4
khoane03 <khoatk4444@gmail.com>
2025-06-26 23:07:35 +0700

    optimize

--- src/pages/chat/ChatBox.tsx
+++ src/pages/chat/ChatBox.tsx
@@ -131,9 +131,9 @@ const ChatBox = ({ userId }: ChatBoxProps) => {
   };
 
   return (
-    <div className="md:flex fixed flex-col h-[calc(100vh - 30px)] w-full border border-gray-200 md:rounded-2xl md:shadow-xl overflow-hidden bg-white my-2">
+    <div className="md:flex flex-col h-[calc(100vh - 30px)] w-full border border-gray-200 md:rounded-2xl md:shadow-xl overflow-hidden bg-white my-2">
       {/* Header */}
-      <div className="bg-gradient-to-r from-purple-500 to-purple-700 text-white px-4 py-3 flex items-center gap-4 shadow-md">
+      <div className="bg-gradient-to-r from-purple-500 to-purple-700 text-white px-4 py-3 flex items-center md:gap-4 shadow-md">
         {/* Back button với hiệu ứng hover */}
         <div onClick={() => navigate('/chat')}
           className="w-10 h-10 md:hidden flex items-center justify-center rounded-full bg-white/20 hover:bg-white/30 cursor-pointer transition-all"
